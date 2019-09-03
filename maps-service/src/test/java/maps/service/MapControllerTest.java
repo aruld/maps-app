9cf85637791b0bd41f9b60dda0188c1eb0f035e0
Arul Dhesiaseelan <aruld.info@gmail.com>
2019-09-02 23:18:57 -0500

    updated to a junit test

--- maps-service/src/test/java/maps/service/MapControllerTest.java
+++ maps-service/src/test/java/maps/service/MapControllerTest.java
@@ -5,29 +5,27 @@ import io.micronaut.core.type.Argument;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.runtime.server.EmbeddedServer;
-import io.micronaut.test.annotation.MicronautTest;
 import maps.common.DirectionDTO;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-@MicronautTest
 public class MapControllerTest {
 
     private static EmbeddedServer server;
     private static HttpClient client;
 
-    @BeforeAll
+    @BeforeClass
     public static void setupServer() {
         server = ApplicationContext.run(EmbeddedServer.class);
         client = server.getApplicationContext().createBean(HttpClient.class, server.getURL());
     }
 
-    @AfterAll
+    @AfterClass
     public static void stopServer() {
         if (server != null) {
             server.stop();
@@ -38,12 +36,12 @@ public class MapControllerTest {
     }
 
     @Test
-    public void getDirections() {
+    public void testDirections() {
         List<DirectionDTO> directions = client.toBlocking().retrieve(HttpRequest.GET("/apple?src=apple&dest=google"), Argument.listOf(DirectionDTO.class));
         assertEquals(6, directions.size());
 
         directions = client.toBlocking().retrieve(HttpRequest.GET("/fastest?src=apple&dest=google"), Argument.listOf(DirectionDTO.class));
-        assertEquals(6, directions.size());
+        assertEquals(7, directions.size());
     }
 
 }
